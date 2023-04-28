be20387855cef184a9224174e0df726dabf6b921
Geshata <64840882+g3shaa@users.noreply.github.com>
2023-04-28 19:25:35 +0300

    Categories

--- src/script.js
+++ src/script.js
@@ -1,14 +1,30 @@
-const apiKey = 'YOUR-PEXELS-API';
+const apiKey = 'YOUR-API-PEXELS-KEY';
 
 const form = document.getElementById('search-form');
 const input = document.getElementById('search-input');
 const container = document.getElementById('photos-container');
 
+// добавяме event listeners за категории
+const categories = document.querySelectorAll('.category');
+categories.forEach(category => {
+    category.addEventListener('click', (e) => {
+        e.preventDefault();
+        const query = category.dataset.category;
+        searchPhotos(query);
+    });
+});
+
+// добавяме event listener за търсене
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const query = input.value;
 
+    searchPhotos(query);
+});
+
+// функция за търсене на снимки
+function searchPhotos(query) {
     fetch(`https://api.pexels.com/v1/search?query=${query}&per_page=24`, {
             headers: {
                 Authorization: apiKey
@@ -19,10 +35,10 @@ form.addEventListener('submit', (e) => {
             const photos = data.photos;
             if (photos.length > 0) {
                 const resultsHTML = photos.map(photo => `
-            <div class="photo">
-            <img src="${photo.src.medium}" alt="${photo.photographer}">
-            </div>
-          `).join('');
+          <div class="photo">
+          <img src="${photo.src.medium}" alt="${photo.photographer}">
+          </div>
+        `).join('');
                 container.innerHTML = resultsHTML;
 
                 // добавяме event listener за кликване върху снимките
@@ -31,20 +47,19 @@ form.addEventListener('submit', (e) => {
                     img.addEventListener('click', () => {
                         const overlay = document.createElement('div');
                         overlay.classList.add('overlay');
-                        const imgElement = document.createElement('img');
-                        const imgUrl = img.getAttribute('src').replace('/medium/', '/large/');
-                        imgElement.setAttribute('src', imgUrl);
-                        overlay.appendChild(imgElement);
-                        document.body.appendChild(overlay);
+                        const imgOverlay = document.createElement('img');
+                        imgOverlay.src = img.src;
+                        overlay.appendChild(imgOverlay);
+                        container.appendChild(overlay);
+                        // добавяме event listener за затваряне на оверлея
                         overlay.addEventListener('click', () => {
                             overlay.remove();
                         });
                     });
                 });
             } else {
-                alert('No results found. Please try again with a different keyword.');
+                container.innerHTML = '<p>No results found</p>';
             }
         })
-        .catch(error => console.error(error));
-});
-
+        .catch(error => console.log(error));
+}
