9870ba70d653c193ed2e54c38e0e50a2d366a2de
Geshata <64840882+g3shaa@users.noreply.github.com>
2023-04-28 16:59:19 +0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-const apiKey = 'YOUR-FLICKR-API';
+const apiKey = 'YOUR-PEXELS-API';
 
 const form = document.getElementById('search-form');
 const input = document.getElementById('search-input');
@@ -9,14 +9,18 @@ form.addEventListener('submit', (e) => {
 
     const query = input.value;
 
-    fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
+    fetch(`https://api.pexels.com/v1/search?query=${query}&per_page=24`, {
+        headers: {
+            Authorization: apiKey
+        }
+    })
         .then(response => response.json())
         .then(data => {
-            const photos = data.photos.photo;
+            const photos = data.photos;
             if (photos.length > 0) {
                 const resultsHTML = photos.map(photo => `
             <div class="photo">
-              <img src="https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_q.jpg" alt="${photo.title}">
+              <img src="${photo.src.medium}" alt="${photo.photographer}">
             </div>
           `).join('');
                 container.innerHTML = resultsHTML;
@@ -28,7 +32,7 @@ form.addEventListener('submit', (e) => {
                         const overlay = document.createElement('div');
                         overlay.classList.add('overlay');
                         const imgElement = document.createElement('img');
-                        const imgUrl = img.getAttribute('src').replace('_q.jpg', '_b.jpg');
+                        const imgUrl = img.getAttribute('src').replace('/medium/', '/large/');
                         imgElement.setAttribute('src', imgUrl);
                         overlay.appendChild(imgElement);
                         document.body.appendChild(overlay);
